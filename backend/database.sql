464fbf19215510adf087d1a6454d2367cc9c2e02
Mujipro1 <mshafqat.bscs21seecs@seecs.edu.pk>
2025-02-27 16:39:21 +0500

    changed db

--- backend/database.sql
+++ backend/database.sql
@@ -123,7 +123,7 @@ CREATE TABLE `pricing` (
   `pricing_id` int NOT NULL AUTO_INCREMENT,
   `plan_name` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `description` json DEFAULT NULL,
+  `description` text DEFAULT NULL,
   PRIMARY KEY (`pricing_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;

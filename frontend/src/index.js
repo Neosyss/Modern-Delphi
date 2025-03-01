591b5365331f2d14426f2445b4bf54a8d9ebe31e
Mujipro1 <mshafqat.bscs21seecs@seecs.edu.pk>
2025-03-02 03:45:54 +0500

    fixed stupid error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App1 from './Pages/Global/NewChatBot';
 
 import 'jquery';
 import 'popper.js';

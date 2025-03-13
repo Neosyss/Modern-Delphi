ecdb44c285fe67a751581d41679445851dabd39f
talal-codora <talal.farrukh@codora.io>
2025-03-14 02:23:17 +0500

    footer changes

--- frontend/src/Pages/Home/FooterHome.js
+++ frontend/src/Pages/Home/FooterHome.js
@@ -8,19 +8,18 @@ const FooterHome = () => {
             <div className="footer-main py-5">
                 <div className="row p-4 pt-4">
 
-                    <div className="col-md-5 mt-3 px-4">
+                    <div className="col-md-4 mt-3 px-4">
                         <div className="d-flex flex-column align-items-center">
                             <p className="p text-dark fw-bold">Modern Delphi</p>
-                            <div className="">
+                            <div className="text-center">
                                 <div className="f-link grey" onClick={() => {navigate('/journeyers-anteroom')}}>Journeyer's Anteroom</div>
                                 <div className="f-link grey" onClick={() => {navigate('/sacred-library')}}>Sacred Library</div>
                                 <div className="f-link grey" onClick={() => {navigate('/consult-oracle')}}>Consult the Oracle</div>
                                 <div className="f-link grey" onClick={() => {navigate('/about-us')}}>About Us</div>
                             </div>
                         </div>
-
                     </div>
-                    <div className="col-md-7 mt-3 px-4">
+                    <div className="col-md-4 mt-3 px-4">
                         <p className="p text-dark text-center fw-bold">Reach Out to Us</p>
                         <div className="my-4 text-center">
                             <div className="f-link grey">contact@moderndelphi.com</div>
@@ -28,6 +27,13 @@ const FooterHome = () => {
                             <div className="f-link grey" onClick={() => {navigate('/terms-of-use')}}>Terms of Use and Disclaimers</div>
                         </div>
                     </div>
+
+                    <div className="col-md-4 mt-3 px-4">
+                        <p className="p text-dark text-center">Modern Delphi harnesses the wisdom of both <br/> Human and Artificial Intelligence</p>
+                    </div>
+
+                    <div className="d-flex flex-column align-items-center"></div>
+
 
                 </div>
             </div>
